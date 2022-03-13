ac37387d50506eb0ea83aca4b1a672f9d3701a69
alucio06 <alucio2106@gmail.com>
2022-03-13 15:32:54 -0300

    Seção 5: Angular - Interpolação, Diretivas e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltered: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImages = true;
+  filterKeyword: string = '';
+
+  filterEvents() : any {
+    let filterBy = this.filterKeyword.toLowerCase();
+    this.eventosFiltered = this.eventos.filter(
+      ( evento: { tema: string; local:string; }) =>
+          evento.tema.toLowerCase().startsWith(filterBy) ||
+          evento.local.toLowerCase().startsWith(filterBy)
+    );
+  }
+
+  // public get filterList(): string {
+  //   return this._filterKeyword;
+  // }
+
+  // public set filterList(value: string) {
+  //   this._filterKeyword = value;
+  //   this.eventos = this.filterList ? this.filterEvents(this.filterList): this.eventos;
+  // }
+
+  // filterEvents(filterBy: string): any {
+  //   filterBy : filterBy.toLocaleLowerCase();
+  //   return this.eventos.filter(
+  //     ( evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1
+  //   )
+  // }
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +49,16 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltered = response
+      },
       error => console.log(error)
     );
+  }
+
+  toggleImagesVisibilty() : void {
+    this.showImages = !this.showImages;
   }
 
 }
